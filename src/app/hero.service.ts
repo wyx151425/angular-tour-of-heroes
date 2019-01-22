d0c643c7e2c5fce05ed0a36e1f43a40476000dd3
wyx151425 <wyx151425@foxmail.com>
2019-01-22 08:09:50 +0800

    2019/01/22 开启版本控制

---
+++ src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {HEROES} from './mock-heroes';
+import {Hero} from './hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() {
+  }
+
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+}
